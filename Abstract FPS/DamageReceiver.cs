b14f5c965b8b88045ff01be4f5e67221cc3c7d24
0dminnimda <52697657+0dminnimda@users.noreply.github.com>
2021-06-07 05:30:22 +0300

    DamageReceiver.cs: implement healthStorage, TakeDamage, infoImportantForDamage

--- Abstract FPS/DamageReceiver.cs
+++ Abstract FPS/DamageReceiver.cs
@@ -7,6 +7,14 @@ namespace Abstract_FPS
 {
     public abstract class DamageReceiver : IDamageReceiver
     {
+        public int healthStorage { get => throw new NotImplementedException(); }
+        public int infoImportantForDamage { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void TakeDamage()
+        {
+            throw new NotImplementedException();
+        }
+
         public void TakeDamageFrom(IDamageDealer damageDealer)
         {
             throw new NotImplementedException();
