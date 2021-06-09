c37046e26120beaa2c3b3f63e8113a0e138fe996
0dminnimda <52697657+0dminnimda@users.noreply.github.com>
2021-06-09 04:38:07 +0300

    Create RepeatedDamagePattern.cs

---
+++ AbstractGame_CSarp/DamageSystem/RepeatedDamagePattern.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DamageSystem
+{
+    // i start thinking that we can just use functions instead
+
+    class RepeatedDamagePattern : IDamagePattern
+    {
+        readonly int numberOfRepetitions;
+        readonly int timeBetweenDamage;
+        readonly int damage;
+
+        public RepeatedDamagePattern(int numberOfRepetitions, int timeBetweenDamage, int damage)
+        {
+            this.numberOfRepetitions = numberOfRepetitions;
+            this.timeBetweenDamage = timeBetweenDamage;
+            this.damage = damage;
+        }
+
+        public void ApplyOn(IDamageReceiver damageReceiver)
+        {
+            for (var i = 0; i < numberOfRepetitions; i++)
+            {
+                // damage
+                damageReceiver.TakeDamage();
+
+                // wait timeBetweenDamage seconds ..
+            }
+        }
+    }
+}
